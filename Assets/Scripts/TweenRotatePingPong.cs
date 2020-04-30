35787d443ebb75d063ea12008a3d051b4c8b6cc4
Lauris Taube <lauristaube@gmail.com>
2020-04-30 14:54:47 +0300

    Updated main menu logo

---
+++ Assets/Scripts/TweenRotatePingPong.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TweenRotatePingPong : MonoBehaviour
+{
+    Vector3 m_Rotation = new Vector3(0f, 0f, -20f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(m_Rotation, 2f).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
